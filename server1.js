4684fa585f25c3fad7733eb6354fe67c62a25d92
kd4 <kd4@kd4.com>
2019-04-26 21:12:56 +0530

    first server file

--- server1.js
+++ server1.js
@@ -14,68 +14,6 @@ app.use(function(request, response, next) {
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-function dbConnection()
-{
-    const connection = mysql.createConnection({
-            host:'localhost',
-            user:'root',
-            password:'manager',
-            database:'sunbeamDB'
-    });
-    connection.connect();
-    return connection;
-}
-
-app.get('/emp',(request,response) => {
-
-    const connection = dbConnection();
-   
-    const statement = 'select * from emp';
-    connection.query(statement,(error,emp)=>{
-        connection.end();
-        response.send(emp);
-
-    });
-});
-
-
-app.post('/emp',(request,response) => {
-    const data = request.body;
-    const connection = dbConnection();
-   
-    const statement =`insert into emp values ('${data.No}','${data.Name}','${data.City}')`;
-    connection.query(statement,(error,emp1)=>{
-        connection.end();
-        response.send(emp1);
-
-    });
-
-});
-
-app.put('/emp/:No',(request,response) => {
-    const id = request.params.No;
-    const data = request.body;
-
-    const connection = dbConnection();
-    const statement = `update emp set Name = '${data.Name}', City = '${data.City}' where No = ${id}`;
-    connection.query(statement, (error, result) => {
-        connection.end();
-        response.send(result);
-    })
-
-});
-
-app.delete('/emp/:id',(request,response) => {
-    const data = request.body;
-    const id = request.params.id;
-    const connection = dbConnection();
-   
-    const statement =`delete from emp where No= ${id}`;
-    connection.query(statement,(error,emp3)=>{
-        connection.end();
-        response.send(emp3);
-
-    });
 
 });
 app.listen(4000,'0.0.0.0',() => {
